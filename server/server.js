d9dc9ccf559d2bcc75384a86740a2143267fdfcb
Adrian Veppo <veppoadrian98@gmail.com>
2023-10-05 11:56:00 -0300

    Seventh commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const Stripe = require('stripe')
 const cors = require('cors')
-
+const REACT_APP_STRIPE_KEY = require('./keys.js')
 
 app = express()
 
-const stripe = new Stripe('sk_test_51MtHEKIyp0hRR6qrcyGtrci4YfX8L5mbibLGrpTXk9jGupCla0K0lAG6mQCUdUTmAKQ1Lc8yOsDkzrlK8r1lcY7Z00mOwkpOsb')
+const stripe = new Stripe(REACT_APP_STRIPE_KEY)
 
 app.use(cors({origin: 'http://localhost:3000'}))
 
